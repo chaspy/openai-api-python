d3456a70b061cc380c08513350462b2705c29b8f
kondo takeshi <chaspy@users.noreply.github.com>
2023-11-11 22:00:32 +0900

    call api

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+from openai import OpenAI
+
+client = OpenAI()  # load OPENAI_API_KEY from environment variable
+
+chat_completions = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "あなたはプロのスタジオミュージシャンです。"},
+        {
+            "role": "user",
+            "content": "初心者がドラムを練習する方法を教えてください"},
+    ],
+    model="gpt-3.5-turbo",
+)
+print(chat_completions)
